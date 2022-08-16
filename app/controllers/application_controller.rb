dbf443293cf5d40e7755be8919fcd2b4aec21bd9
kanchan <kanchankamboj1020@gmail.com>
2022-08-16 12:25:21 +0530

    Signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :account_type)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit( :email, :password, :password_confirmation, :account_type)}
+  end
+
 	def after_sign_in_path_for(resource)
-    cakes_index_path
+    cakes_path
   end
 end
