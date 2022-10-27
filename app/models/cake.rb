fb9fd1c20cc52a20185bcf376e1488701a4270f4
Shweta <shwetagogia60@gmail.com>
2022-10-27 18:28:07 +0530

    UI Changes

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -2,4 +2,5 @@ class Cake < ApplicationRecord
   enum category: [:birthday, :wedding, :custom]
   has_many :line_items, dependent: :destroy
   has_one_attached :image
+  validates :name, presence: true, uniqueness: true
 end
